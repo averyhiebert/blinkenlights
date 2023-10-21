d3914272421a68e04f96ed99725790619110fc68
RyanSamman <RyanSamman@outlook.com>
2023-10-21 01:05:08 -0400

    Blink Detection, Frame Limit, and Debug Menu

--- experiments/face-landmarker-testing/script.js
+++ experiments/face-landmarker-testing/script.js
@@ -3,16 +3,27 @@ import { FaceLandmarker, FilesetResolver, DrawingUtils } from "https://cdn.jsdel
 
 let isDebugMode = true;
 let isActiveAI = false;
-const videoWidth = 480;
 
 const windowElementDebug = document.getElementById("debug")
 const windowElementVideo = document.getElementById("webcam");
 const windowElementCanvas = document.getElementById("output_canvas");
 const videoBlendShapes = document.getElementById("video-blend-shapes");
 
-let aiFaceLandmarker = null;
 const canvasCtx = windowElementCanvas.getContext("2d");
+const drawingUtils = new DrawingUtils(canvasCtx);
 
+function initializeVideoCanvas() {
+    const videoWidth = 480;
+    const ratio = windowElementVideo.videoHeight / windowElementVideo.videoWidth;
+    windowElementVideo.style.width = videoWidth + "px";
+    windowElementVideo.style.height = videoWidth * ratio + "px";
+    windowElementCanvas.style.width = videoWidth + "px";
+    windowElementCanvas.style.height = videoWidth * ratio + "px";
+    windowElementCanvas.width = windowElementVideo.videoWidth;
+    windowElementCanvas.height = windowElementVideo.videoHeight;
+}
+
+let aiFaceLandmarker = undefined;
 windowElementDebug.addEventListener("click", toggleDebugMode)
 
 function toggleDebugMode() {
@@ -30,68 +41,103 @@ function pageSupportsWebcam() {
     return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
-let lastVideoTime = -1;
-let results = undefined;
-const drawingUtils = new DrawingUtils(canvasCtx);
-async function predictWebcam() {
-    const radio = windowElementVideo.videoHeight / windowElementVideo.videoWidth;
-    windowElementVideo.style.width = videoWidth + "px";
-    windowElementVideo.style.height = videoWidth * radio + "px";
-    windowElementCanvas.style.width = videoWidth + "px";
-    windowElementCanvas.style.height = videoWidth * radio + "px";
-    windowElementCanvas.width = windowElementVideo.videoWidth;
-    windowElementCanvas.height = windowElementVideo.videoHeight;
-    // Now let's start detecting the stream.
-    let startTimeMs = performance.now();
-    if (lastVideoTime !== windowElementVideo.currentTime) {
-        lastVideoTime = windowElementVideo.currentTime;
-        results = aiFaceLandmarker.detectForVideo(windowElementVideo, startTimeMs);
-    }
 
-    if (results.faceLandmarks) {
-        for (const landmarks of results.faceLandmarks) {
+const BLINK_THRESHHOLD = 0.6
+const AI_TICK_RATE = 50; // MS
+
+let lastVideoTime = -1;
+let lastTimestamp = performance.now()
+let timeElapsedAI = 0.0;
+let timeElapsedOverall = 0.0;
+let eyeBlinkLeftData = undefined;
+let eyeBlinkRightData = undefined;
+
+let wasBlinking = false;
+let isBlinking = false;
+
+async function predictWebcam() {
+    let results = undefined;
+
+    // just in case for any errors
+    try {
+        // FIXME: Why here? putting this anywhere else stops the face blendshapes from forming 
+        initializeVideoCanvas()
+        
+        let startTimeMs = performance.now();
+        if (lastVideoTime !== windowElementVideo.currentTime) {
+            lastVideoTime = windowElementVideo.currentTime;
+            results = aiFaceLandmarker.detectForVideo(windowElementVideo, startTimeMs);
+        }
+
+        let currentTimestamp = performance.now()
+        timeElapsedAI = currentTimestamp - startTimeMs
+        timeElapsedOverall = currentTimestamp - lastTimestamp 
+        lastTimestamp = currentTimestamp
+
+        // Results is whether a face is present
+        isActiveAI = !!results
+
+        // Render Eye Landmarks onto canvas
+        if (results?.faceLandmarks) {
+            const landmarks = results.faceLandmarks[0]
             drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE, { color: "#FF3030" });
             drawingUtils.drawConnectors(landmarks, FaceLandmarker.FACE_LANDMARKS_LEFT_EYE, { color: "#30FF30" });
         }
-    }
-    drawBlendShapes(videoBlendShapes, results.faceBlendshapes);
 
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
+        // Stats
+        if (results?.faceBlendshapes?.length) {
+            isActiveAI = true;
+            let faceData = results.faceBlendshapes[0];
+            eyeBlinkLeftData = faceData.categories[9]
+            eyeBlinkRightData = faceData.categories[10]
+
+            let leftEyeBlinkPerc = eyeBlinkLeftData.score
+            let rightEyeBlinkPerc = eyeBlinkRightData.score
+
+            if (leftEyeBlinkPerc > BLINK_THRESHHOLD && rightEyeBlinkPerc > BLINK_THRESHHOLD) {
+                isBlinking = true
+            } 
+
+            if (leftEyeBlinkPerc < BLINK_THRESHHOLD || rightEyeBlinkPerc < BLINK_THRESHHOLD) {
+                isBlinking = false;
+            }
+
+            if (!wasBlinking && isBlinking) {
+                console.log("Begun to Blink!")
+            } else if (wasBlinking && !isBlinking) {
+                console.log("Stopped Blinking!")
+            }
+
+            wasBlinking = isBlinking
+
+        } else {
+            isActiveAI = false;
+        }
+
+    } catch (e) {
+        console.error(e);
+        isActiveAI = false
+    }
+
 }
-function drawBlendShapes(el, blendShapes) {
-    if (!blendShapes.length) {
-        return;
-    }
-    // console.log(blendShapes[0]);
-    let htmlMaker = "";
-    let eyeBlinkLeftData = blendShapes[0].categories[9]
-    let eyeBlinkRightData = blendShapes[0].categories[10]
+function drawDebugMenu(el, { eyeBlinkLeftData, eyeBlinkRightData, timeElapsedAI, timeElapsedOverall, blinkThreshhold, isActiveAI }) {
+    let htmlString = "";
     
-    function debugHTML() {
-        htmlMaker = "";
+    const htmlMacro = (title, value, width=0) => (
+        `<li class="blend-shapes-item">
+        <span class="blend-shapes-label">${title}</span>
+        <span class="blend-shapes-value" style="width: calc(${width}% - 100px)">${value}</span>
+        </li>`
+    )
 
-        htmlMaker += `<li class="blend-shapes-item">
-        <span class="blend-shapes-label">AI Active</span>
-        <span class="blend-shapes-value"  style="width: calc(${isActiveAI ? 100 : 0}% - 120px)"">${isActiveAI}</span>
-      </li>`
+    htmlString += htmlMacro("Overall Time:",`${(timeElapsedOverall).toFixed(0)}ms`)
+    htmlString += htmlMacro("AI Time:",`${(timeElapsedAI).toFixed(0)}ms`)
+    htmlString += htmlMacro("Is AI Active?",isActiveAI)
+    htmlString += htmlMacro(eyeBlinkLeftData.displayName || eyeBlinkLeftData.categoryName, (+eyeBlinkLeftData.score).toFixed(4), eyeBlinkLeftData.score * 100)
+    htmlString += htmlMacro(eyeBlinkRightData.displayName || eyeBlinkRightData.categoryName, (+eyeBlinkRightData.score).toFixed(4), eyeBlinkRightData.score * 100)
+    htmlString += htmlMacro("Blink Thold:",`${(blinkThreshhold).toFixed(4)}`)
 
-        const htmlMacro = (shape) => (
-            `<li class="blend-shapes-item">
-        <span class="blend-shapes-label">${shape.displayName || shape.categoryName}</span>
-        <span class="blend-shapes-value" style="width: calc(${+shape.score * 100}% - 120px)">${(+shape.score).toFixed(4)}</span>
-      </li>`
-        )
-
-
-        htmlMaker += htmlMacro(eyeBlinkLeftData)
-        htmlMaker += htmlMacro(eyeBlinkRightData)
-
-        return htmlMaker
-
-    }
-
-    el.innerHTML = debugHTML()
+    el.innerHTML = htmlString;
 }
 
 
@@ -143,8 +189,25 @@ async function main() {
     isActiveAI = true;
     console.log(stream)
 
+
+    function tickAI() {
+        if (stream.active) {
+            predictWebcam()
+        } else {
+            isActiveAI = false
+        }
+
+        if (isDebugMode) {
+            drawDebugMenu(videoBlendShapes, { eyeBlinkLeftData, eyeBlinkRightData, timeElapsedAI, timeElapsedOverall, blinkThreshhold: BLINK_THRESHHOLD, isActiveAI });
+        }
+    }
+
     windowElementVideo.srcObject = stream
-    windowElementVideo.addEventListener("loadeddata", predictWebcam)
+    windowElementVideo.addEventListener("loadeddata", () => { 
+        tickAI()
+        setInterval(tickAI, AI_TICK_RATE) 
+    })
+
 
     console.log("Finished Initialization")
 
